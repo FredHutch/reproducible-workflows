67df8bf4105246ec8a766c7fdf7e01b9cda5911f
Sam Minot <sam@fredhutch.org>
2019-01-04 12:03:25 -0800

    Testing works with S3 / AWS Batch

--- WDL/align-proteins-diamond/run.sh
+++ WDL/align-proteins-diamond/run.sh
@@ -1,1 +1,1 @@
-java -Dconfig.file=$HOME/.cromwell/aws.conf -jar $HOME/.cromwell/cromwell-36.jar run denovo-gene-level-metagenomics-cags.wdl -i denovo-gene-level-metagenomics-cags.inputs.json
+java -Dconfig.file=$HOME/.cromwell/aws.conf -jar $HOME/.cromwell/cromwell-36.jar run align-proteins-diamond.wdl -i align-proteins-diamond.inputs.json
